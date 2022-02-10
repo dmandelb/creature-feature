5105bcc606e0c8c318d0d231f01d31c91337adb8
dmandelb <devin.mandelbaum@gmail.com>
2022-02-10 13:58:52 -0500

    Add single creature component file

---
+++ src/Creature.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Creature extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <li></li>
+    );
+  }
+}
+ 
+export default Creature;
